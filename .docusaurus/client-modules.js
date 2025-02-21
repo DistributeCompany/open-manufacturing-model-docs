70b0d8f418f5f920d29f5d72347f2eef77909483
DistributeCompany <121041633+DistributeCompany@users.noreply.github.com>
2025-02-21 09:15:54 +0100

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\coding\\open-manufacturing-model-docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\coding\\open-manufacturing-model-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\coding\\open-manufacturing-model-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\coding\\open-manufacturing-model-docs\\src\\css\\custom.css"),
-];
